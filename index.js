c531f83e6bf3582bf6c253fb141d34ec04fee33a
Shahen Hovhannisyan <shahen@simplytechnologies.net>
2016-09-29 15:51:25 +0400

    updated hexagonImage

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { View, Image, requireNativeComponent } from 'react-native';
 
-const HexagonImage = ({
-  src,
-  borderWidth,
-  borderColor,
-  accessibilityLabel,
-  children
-}) => (
-  <NativeRNShapeImageView
-    src={src}
-    borderWidth={borderWidth}
-    borderColor={borderColor}
-    accessibilityLabel={accessibilityLabel}
-    testID={'hexagonImageView'}
-  >
-    {children}
-  </NativeRNShapeImageView>
-);
 
-HexagonImage.propTypes = {
-  src: PropTypes.string.isRequired,
-  borderWidth: PropTypes.number,
-  borderColor: PropTypes.string,
-  ...Image.propTypes
-};
+export class HexagonImage extends Component {
+  static propTypes = {
+    src: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.number,
+      PropTypes.string
+    ]).isRequired,
+    borderWidth: PropTypes.number,
+    borderColor: PropTypes.string,
+    ...Image.propTypes
+  }
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  componentWillMount() {
+    const { src } = this.props;
+    console.log(src);
+  }
+
+  render() {
+    const {
+      children,
+      src,
+      borderWidth,
+      borderColor,
+      ...props
+    } = this.props;
+    return (
+      <NativeRNShapeImageView
+        src={src}
+        borderWidth={borderWidth}
+        borderColor={borderColor}
+        {...props}
+      >
+        {children}
+      </NativeRNShapeImageView>
+    );
+  }
+}
 
 const NativeRNShapeImageView = requireNativeComponent('RNShapeImageView', HexagonImage);
-
-export {
-  HexagonImage
-}
