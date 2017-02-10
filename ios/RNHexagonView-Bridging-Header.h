2e2e913ceebb52c3630538957eb00ee504b3a242
Shahen Hovhannisyan <shahen@simplytechnologies.net>
2017-02-10 20:59:09 +0400

    rn 0.41 support

--- ios/RNHexagonView-Bridging-Header.h
+++ ios/RNHexagonView-Bridging-Header.h
@@ -1,6 +1,6 @@
 #ifndef RNHexagonView_Bridging_Header_h
 #define RNHexagonView_Bridging_Header_h
 
-#import "RCTBridgeModule.h"
+#import "React/RCTBridgeModule.h"
 
 #endif /* RNHexagonView_Bridging_Header_h */
--- ios/RNHexagonViewBridge.m
+++ ios/RNHexagonViewBridge.m
@@ -4,8 +4,8 @@
 
 #import <Foundation/Foundation.h>
 
-#import "RCTBridgeModule.h"
-#import "RCTViewManager.h"
+#import "React/RCTBridgeModule.h"
+#import "React/RCTViewManager.h"
 
 @interface RCT_EXTERN_MODULE(RNHexagonViewManager, RCTViewManager)
 
